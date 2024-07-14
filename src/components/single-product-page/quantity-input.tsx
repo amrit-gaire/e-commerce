eb6a73b18bd02aaaf5a33ae3330d4ac78f720f2a
GosuCode <gosucode1945@gmail.com>
2024-07-14 17:33:03 +0545

    update qunatity on click

--- src/components/single-product-page/quantity-input.tsx
+++ src/components/single-product-page/quantity-input.tsx
@@ -13,6 +13,9 @@ const QuantityInput = (props: Props) => {
   const pathname = usePathname();
   const router = useRouter();
 
+  const MIN_QUANTITY = 1;
+  const MAX_QUANTITY = 5;
+
   const [count, setCount] = useState(
     parseInt(searchParams.get(SELECTED_QUANTITY) ?? "1")
   );
@@ -27,35 +30,53 @@ const QuantityInput = (props: Props) => {
     [searchParams]
   );
 
-  const updateCount = (newCount: number) => {
-    setCount(newCount);
-    router.push(
-      pathname + "?" + createQueryString(SELECTED_QUANTITY, newCount.toString())
-    );
+  const inc = () => {
+    if (count < MAX_QUANTITY) {
+      setCount(count + 1);
+      router.push(
+        pathname +
+          "?" +
+          createQueryString(SELECTED_QUANTITY, (count + 1).toString())
+      );
+    }
   };
 
-  const handleQuantityUpdate = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = Math.max(1, parseInt(event.target.value));
-    updateCount(value);
+  const dec = () => {
+    if (count > MIN_QUANTITY) {
+      setCount(count - 1);
+      router.push(
+        pathname +
+          "?" +
+          createQueryString(SELECTED_QUANTITY, (count - 1).toString())
+      );
+    }
+  };
+
+  const handleQuantiyUpdate = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = Math.min(
+      Math.max(parseInt(e.target.value), MIN_QUANTITY),
+      MAX_QUANTITY
+    );
+    setCount(value);
+    router.push(
+      pathname + "?" + createQueryString(SELECTED_QUANTITY, value.toString())
+    );
   };
 
   return (
     <div className="flex">
-      <Button
-        variant={"outline"}
-        onClick={() => updateCount(Math.max(1, count - 1))}
-      >
+      <Button variant={"outline"} onClick={dec}>
         -
       </Button>
       <input
         type="number"
-        min={1}
-        max={5}
+        min={MIN_QUANTITY}
+        max={MAX_QUANTITY}
         className="focus:outline-none px-3 py-2 w-[4ch] text-center"
         value={count}
-        onChange={handleQuantityUpdate}
+        onChange={handleQuantiyUpdate}
       />
-      <Button variant={"outline"} onClick={() => updateCount(count + 1)}>
+      <Button variant={"outline"} onClick={inc}>
         +
       </Button>
     </div>
